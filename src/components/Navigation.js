2e55b4762716f41c7588bb5f4c1f5cf47e3a0b62
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-01-13 14:39:14 +0530

    navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import '../App.css'
+import '../Stylesheets/navigation.css'
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export const Navigation = () => {
     return (
@@ -10,7 +18,33 @@ export const Navigation = () => {
                 <div id="line3"></div>
             </div>
             <div className="logo">
-                <img src ={"../youtube_logo_dark.jpg"}></img>
+                <YouTubeIcon className="yt-icon" style={{ fontSize:30 }} />
+                <div className="yt-icon-label">YouTube
+                    <sup className="india">IN</sup>
+                </div>
+            </div>
+            <div className="search-bar">
+                <input className="search" placeholder="Search"></input>
+            </div>
+            <div className="search-lens">
+                <SearchIcon className="lens" style={{fontSize: 20, color: "rgb(140, 140, 140)"}} />
+            </div>
+            <div className="mic">
+                <MicIcon style={{color:"white"}} />
+            </div>
+            <div className="accessories">
+                <div className="add-video">
+                    <VideoCallIcon style={{color:"white"}} />
+                </div>
+                <div className="yt-apps">
+                    <AppsIcon style={{color:"white"}} />
+                </div>
+                <div className="bell-icon">
+                    <NotificationsIcon style={{color:"white"}} />
+                </div>
+                <div className="profile">
+                    <AccountCircleIcon style={{color:"white", fontSize:40}} />
+                </div>
             </div>
         </div>
     )
